5d77eb97c3c939b8b4de75ae7a66ef4763cbd465
Kaspar Triebstok <kaspar@nordic.id>
2018-04-13 22:51:27 +0700

    ...

--- src/functions.js
+++ src/functions.js
@@ -2,11 +2,10 @@ import axios from 'axios'
 import dummyResponse from './assets/dummy-img-label-response.json'
 
 let apiKey = 'ASsdtD34sd23daWedas84f3aHe'
-let serverRemote = 'http://54.158.57.162/nordic/image'
 let serverLocal = 'http://0.0.0.0:8090/image'
 
 export const labelImage = (imgUrl) => {
-  var requestUrl = serverRemote + '?imgUri=' + imgUrl + '&key=' + apiKey
+  var requestUrl = serverLocal + '?imgUri=' + imgUrl + '&key=' + apiKey
   console.log('requesting data from:' + requestUrl)
 
   return axios.get(requestUrl)
